d0bcd4bd78e25a9f72a61460de4fc8e9d879c3ad
myferr <contactme.myfer@protonmail.com>
2026-01-05 16:06:15 -0600

    feat: add ANSI color constants

---
+++ src/colors.cr
@@ -0,0 +1,20 @@
+module Foxtail
+  VERSION = "0.1.0"
+
+  COLORS = {
+    "\e[31m", # red
+    "\e[32m", # green
+    "\e[33m", # yellow
+    "\e[34m", # blue
+    "\e[35m", # magenta
+    "\e[36m", # cyan
+    "\e[91m", # bright red
+    "\e[92m", # bright green
+    "\e[93m", # bright yellow
+    "\e[94m", # bright blue
+    "\e[95m", # bright magenta
+    "\e[96m", # bright cyan
+  }
+
+  RESET = "\e[0m"
+end
